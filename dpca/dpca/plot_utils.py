36a5e256cd44c796942b9018ef22ec45c05e4f5a
little1d <2431542525@qq.com>
2024-11-12 07:40:49 +0800

    feat: add anormal sample

--- dpca/dpca/plot_utils.py
+++ dpca/dpca/plot_utils.py
@@ -1,7 +1,5 @@
 import numpy as np
-
 import matplotlib.pyplot as plt
-
 from sklearn.manifold import MDS
 from sklearn.preprocessing import scale
 from sklearn.decomposition import PCA
--- dpca/example.py
+++ dpca/example.py
@@ -4,14 +4,14 @@ import pandas as pd
 from dpca import DensityPeakCluster
 
 # file name
-file = "aggregation"
+file = "flame"
 
 # load data
 data = pd.read_csv(r"data/data/%s.txt" % file, sep="\t", header=None)
 
 # dcps model
 # plot decision graph to set params `density_threshold`, `distance_threshold`
-dpca = DensityPeakCluster(density_threshold=8, distance_threshold=5, anormal=False)
+dpca = DensityPeakCluster(density_threshold=8, distance_threshold=5, anormal=True)
 
 
 # fit model
