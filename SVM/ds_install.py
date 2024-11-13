e56644e2133e595d3b923790240b59f08206d1d2
little1d <2431542525@qq.com>
2024-11-13 15:16:48 +0800

    svm init

---
+++ SVM/ds_install.py
@@ -0,0 +1,9 @@
+from datasets import load_dataset
+
+add = ["scikit-learn/iris","mstz/sonar"]
+
+
+for ds in add:
+    print(ds)
+    data = load_dataset(ds, split="train")
+    datasets = data.train_test_split(test_size=0.2, shuffle=True)
